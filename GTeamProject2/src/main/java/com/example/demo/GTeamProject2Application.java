0e28844fbc554f39d693eaa74c6d2677766cfe93
bzahler <maaraken@yahoo.com>
2018-03-07 16:58:23 -0500

    added spring project

---
+++ GTeamProject2/src/main/java/com/example/demo/GTeamProject2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GTeamProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTeamProject2Application.class, args);
+	}
+}

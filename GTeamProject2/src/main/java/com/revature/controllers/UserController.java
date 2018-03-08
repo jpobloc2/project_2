d7ef9f09d5d1241e1c8e13bfb2506ea49c4f81a4
BrandonC <brandoncross@live.com>
2018-03-08 15:42:55 -0500

    ?

--- GTeamProject2/src/main/java/com/revature/controllers/UserController.java
+++ GTeamProject2/src/main/java/com/revature/controllers/UserController.java
@@ -1,21 +1,19 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.revature.entities.LoginCredentials;
 import com.revature.entities.Users;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
 import com.revature.services.UsersServiceInterface;
+import com.revature.views.View;
 
 @RestController
 @RequestMapping("users")
@@ -24,12 +22,15 @@ public class UserController {
 	@Autowired
 	private UsersServiceInterface us;
 
-	
 	@PostMapping 
+	@JsonView(View.UserInfo.class)
 	public Users login(@RequestBody LoginCredentials lc) {
 		System.out.println(lc.getUsername() + " " + lc.getPassword());
 		return us.login(lc.getUsername(), lc.getPassword());
-	}
-	
 
+	@PostMapping("newUser/{manager_id}")
+	public Users createNew(@RequestBody Users u, @PathVariable int manager_id) {
+		return us.createNew(u, manager_id);
+
+	}
 }

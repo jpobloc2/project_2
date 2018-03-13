061128c0e746bb688bb7d8a6606d51fb7b2a82a0
jpobloc2 <jpobloc2@gustavus.edu>
2018-03-12 23:08:24 -0400

    Can now retrieve authentication tokens for users. The code is a mess and there's lots to do.

--- GTeamProject2/src/main/java/com/revature/controllers/UserController.java
+++ GTeamProject2/src/main/java/com/revature/controllers/UserController.java
@@ -1,18 +1,33 @@
 package com.revature.controllers;
 
+import java.util.Objects;
+
+import javax.security.sasl.AuthenticationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.revature.entities.LoginCredentials;
 import com.revature.entities.Users;
+import com.revature.rest.JwtAuthenticationRequest;
+import com.revature.rest.JwtAuthenticationResponse;
 import com.revature.services.UsersServiceInterface;
+import com.revature.util.JwtTokenUtil;
 import com.revature.views.View;
 
 @RestController
@@ -21,6 +36,13 @@ import com.revature.views.View;
 public class UserController {
 	@Autowired
 	private UsersServiceInterface us;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    @Qualifier("jwtUserDetailsService")
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
 	@PostMapping("newUser")
 	@JsonView(View.UserInfo.class)
@@ -36,5 +58,35 @@ public class UserController {
 		System.out.println(lc.getUsername() + " " + lc.getPassword());
 		return us.login(lc.getUsername(), lc.getPassword());
 	}
+	
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException {
 
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        // Reload password post-security so we can generate the token
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        // Return the token
+        return ResponseEntity.ok(new JwtAuthenticationResponse(token));
+    }
+    
+    /**
+     * Authenticates the user. If something is wrong, an {@link AuthenticationException} will be thrown
+     */
+    private void authenticate(String username, String password) throws AuthenticationException {
+        Objects.requireNonNull(username);
+        Objects.requireNonNull(password);
+        System.out.println(username + " " + password);
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new AuthenticationException("User is disabled!", e);
+        } catch (BadCredentialsException e) {
+            throw new AuthenticationException("Bad credentials!", e);
+        }
+    }
 }
+
+
